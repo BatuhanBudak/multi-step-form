4158d2c3e0c4f08231e51f37a6aa871f4fc16841
Batuhan Budak <batuhan.budak@gmail.com>
2022-12-20 22:14:07 +0300

    Remove unused component

--- src/components/FormStepper/FormStepper.tsx
+++
@@ -1,9 +0,0 @@
-// import Stepper from "@mui/material/Stepper";
-// import Step from "@mui/material/Step";
-// import StepLabel from "@mui/material/StepLabel";
-
-// const FormStepper = ({children, ...props}) => {
-//   return <div>FormStepper</div>;
-// };
-
-// export default FormStepper;
